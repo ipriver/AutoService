2d4ec690aa110cd117debaba2e33160abd6dc6b8
ipriver <maxdrozdov18@gmail.com>
2017-04-27 18:41:38 +0300

    functionality and design complete

--- news/forms.py
+++ news/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import About_Comment
+from django.utils.translation import ugettext_lazy as _
 
 
 class CommentForm(forms.Form):
-    title = forms.CharField(label='title', max_length=50)
-    author = forms.CharField(label='author', max_length=50)
-    comment = forms.CharField(label='comment', max_length=250)
-
-    
+    title = forms.CharField(error_messages={'required': ''}, label='Тема комментария:', max_length=50)
+    author = forms.CharField(error_messages={'required': ''}, label='Ваше имя:', max_length=50)
+    comment = forms.CharField(error_messages={'required': ''}, label='Комментарий:', max_length=250, widget=forms.Textarea(attrs={'rows':6,
+                                            'cols':30,
+                                            'style':'resize:none;'}))
